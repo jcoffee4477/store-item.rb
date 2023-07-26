c38e5de0cd66366419d587b8f17a773335256f9f
jcoffee4477 <jacobcoffee@jacobs-air.mynetworksettings.com>
2023-07-26 09:37:37 -0400

    add single option hash

--- store-item.rb
+++ store-item.rb
@@ -5,23 +5,26 @@ class Book
 attr_reader :color, :price, :weight
 attr_writer :color
 
-  def initialize(color, price, weight)
-    @color = color
-    @price = price
-    @weight = weight
+  def initialize(input)
+    @color = input[:color]
+    @price = input[:price]
+    @weight = input[:weight]
+  end
+
+  def info
+    p "the price is "
   end
 
 end
 
 
 
-book1 = Book.new("red", 10, 2)
-book2 = Book.new("blue", 8, 3)
-book3 = Book.new("yellow", 5, 1)
-
+book1 = Book.new(color: "green", price: 3, weight: 4)
+#book2 = Book.new("blue", 8, 3)
+#book3 = Book.new("yellow", 5, 1)
 puts book1.color
-puts book2.weight
-puts book3.price
+puts book1.color= "red"
+
 
 
 
